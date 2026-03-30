98b92a619ad258e1f54bf7b69309527a2524e775
alvesklvn <kelvin.pereira1904@gmail.com>
2026-03-30 11:47:01 -0300

    - adicionada nova rota, importação de rotas alterada em init, link para nova rota integrado em index.html

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/index')
 @app.route('/')
 def homepage():
     return render_template("index.html")
+
+@app.route('/nova')
+def novapag():
+    return 'outra view'
