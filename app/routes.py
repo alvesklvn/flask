6fe5d0704f03faecae5b7ec15ee6a9c4bdd06a26
alvesklvn <kelvin.pereira1904@gmail.com>
2026-04-08 10:39:22 -0300

    Adicionado direcionamento para nova pagina utilizando url_for
    teste
    teste

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
 @app.route('/home')
 def homepage():
     return render_template('index.html')
+
+@app.route('/nova/')
+def novapag():
+    return 'outra view'
